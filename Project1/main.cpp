3e9e56f3f4ca14a1052158085cfd84fc26e5e554
kalai177 <kalaiyarasi.ec19@bitsathy.ac.in>
2022-04-11 16:42:01 +0530

    Userdetails

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,12 +1,29 @@
 #include<iostream>
 #include<Windows.h>
 #include "System.h"
+
+int functionsavailable() {
+	std::cout << "Press the following numbers to use the functions:" << std::endl;
+
+	std::cout << "1 - for creating new user" << std::endl;
+	std::cout << "2 - for showing all users" << std::endl;
+	std::cout << "3 - for delete user" << std::endl;
+	std::cout << "4 - for exit" << std::endl;
+	DWORD func;
+	std::cin >> func;
+	return func;
+}
 int main() {
 	System sys;
 	std::cout << "if you want to add user press 1:";
-	DWORD createuser;
-	std::cin >> createuser;
-	if (createuser == 1) {
+
+functionstarts:
+
+	DWORD executefunc = functionsavailable();
+	switch (executefunc) {
+
+	case 1:
+
 		std::cout << "Enter number of users to be added:";
 		std::cout << std::endl;
 		DWORD num_of_users;
@@ -14,33 +31,35 @@ int main() {
 		for (DWORD itr = 0; itr < num_of_users; itr++) {
 			sys.addusers();
 		}
-	}
+		goto functionstarts;
 
-	DWORD showuser;
-	std::cout << "If you want to view users detail press 1:";
-	std::cin >> showuser;
-	if (showuser == 1) {
+	case 2:
+
 		sys.showusers();
-	}
+		goto functionstarts;
 
-	DWORD deleteuser;
-	std::cout << "If you want ot delete user press 1: ";
-	std::cin >> deleteuser;
-	if (deleteuser == 1) {
-		DWORD how_many_users;
-		std::cout << "Enter how many users you want to delete:";
-		std::cin >> how_many_users;
-		for (DWORD itr = 0; itr < how_many_users; itr++) {
-			sys.deleteusers();
-		}
 
-	}
+	case 3:
+		if (sys.userdetails.size() == 0) {
+			std::cout << "List is empty, there are no users to be deleted" << std::endl;
+			goto functionstarts;
 
-	DWORD exit_program;
-	std::cout << "If you want to exit program press 1: ";
-	std::cin >> exit_program;
-	if (exit_program == 1) {
+		   }
+    		DWORD how_many_users;
+			std::cout << "Enter how many users you want to delete:";
+			std::cin >> how_many_users;
+			for (DWORD itr = 0; itr < how_many_users; itr++) {
+				sys.deleteusers();
+			}
+			goto functionstarts;
+
+	case 4:
+
 		exit(0);
+
+	default:
+		goto functionstarts;
+
 	}
 
 }
